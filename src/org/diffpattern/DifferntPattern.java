3d35e16716d519e883e8f5232d4da048b2436b6a
Lokesh25-kal <lokeshgovindaswamy.eg@gmail.com>
2022-10-17 22:15:11 +0530

    RET2501

---
+++ src/org/diffpattern/DifferntPattern.java
@@ -0,0 +1,18 @@
+package org.diffpattern;
+
+public class DifferntPattern {
+
+	public static void main(String[] args) {
+		// For Loop
+		
+		int row = 4;
+		for(int i = 0;i<row;i++) {
+			for(int j = 0;j<=i;j++) {
+				System.out.print("1");
+			}
+			System.out.println();
+		}
+
+	}
+}
+
